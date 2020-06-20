cfe653c7675cf7c63bb656aa1a95d6576bac2d0d
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-06-20 13:57:48 -0400

    Passed data from axios call to single job/:id route

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class Job extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    componentDidMount = () => {
+        // this.props.match.params gives access to route parameters
+        const jobId = this.props.match.params.id;
+
+        axios.get(`/api/jobs/${jobId}`).then(res => {
+            this.setState(res.data);
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Single Job</h1>
+            <div className="Job">
+                <h1>{this.state.title}</h1>
+                <a><img src={this.state.company_logo} width="100"></img></a>
+                <div>How to apply</div>
+                <span>Job Description</span>
             </div>
         )
     }
