f3e7dac8ce84aeaca821a274d7b1dd9c5964d26d
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-06-20 12:34:09 -0400

    Initial commit with necessary components

---
+++ src/components/JobsListItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './JobsListItem.css';
+
+class JobsListItem extends Component {
+
+    render() {
+        return (
+            <div className="JobsListItem">
+                <div className="left">
+                    <b>Job Title</b>
+                    <div>Company</div>
+                </div>
+                <div className="right">
+                    <b>Type</b>
+                    <div>Timestamp</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JobsListItem;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Jobs from './components/Jobs';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Jobs />
   </React.StrictMode>,
   document.getElementById('root')
 );
